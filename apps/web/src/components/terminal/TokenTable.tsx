91c0172e4554ae0229c35900b35b2072d877cc75
Matthew-Speechley <matthew.speechley@heycar.co.uk>
2026-05-07 14:36:17 +0100

    :art: table formatting, equal cell size and text align

--- apps/web/src/components/terminal/TokenTable.tsx
+++ apps/web/src/components/terminal/TokenTable.tsx
@@ -4,19 +4,12 @@ import TokenRow from "./TokenRow";
 import styles from "./TokenTable.module.css";
 import { useTokens } from "../../hooks/useTokens";
 import { selectActiveFilter } from "../../state/uiSlice";
-import { cn } from "../../utils/format";
 
 function TableHead() {
   return (
     <div className={styles.tableHead}>
-      {["", "TOKEN", "24H", "PROGRESS", "MCAP"].map((h, i) => (
-        <div
-          key={h || i}
-          className={cn(
-            styles.headCell,
-            (i === 2 || i === 4) && styles.headCellRight,
-          )}
-        >
+      {["TOKEN", "24H", "PROGRESS", "MCAP"].map((h) => (
+        <div key={h} className={styles.headCell}>
           {h}
         </div>
       ))}
